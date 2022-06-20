fe4e35eb222c6c03867ce400e98b38d292f0263c
Valentin Borisenko <borison2004@yandex.ru>
2022-06-20 14:52:54 +0700

    resolved

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import hexColorRegex from 'hex-color-regex';
 
-function App() {
+import { ColorForm } from './components/ColorForm';
+import { ColorLabel } from './components/ColorLabel';
+
+const START_COLOR = '#336666';
+const ERROR_COLOR = '#ea4b35';
+const ERROR_TEXT = 'Ошибка!';
+
+const Bacground = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const App: React.FC = () => {
+  const [color, setColor] = useState<string>(START_COLOR);
+  const [isErrorColor, setIsErrorColor] = useState<boolean>(false);
+
+  const changeColorHandler = (color: string) => {
+    if (hexColorRegex().test(color)) {
+      setIsErrorColor(false);
+      setColor(color);
+    } else {
+      setIsErrorColor(true);
+    }
+  };
+
+  const createStyles = () => {
+    const textColor = isErrorColor ? '#ffffff' : color === '#ffffff' ? '#000000' : '#ffffff';
+    const borderColor = isErrorColor ? ERROR_COLOR : color === '#ffffff' ? '#000000' : '#ffffff';
+    const bgColor = isErrorColor ? ERROR_COLOR : color;
+    const labelColor = isErrorColor ? ERROR_COLOR : color;
+    return { textColor, borderColor, bgColor, labelColor };
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Bacground style={{ backgroundColor: createStyles().bgColor }}>
+      <ColorForm onChangeColor={changeColorHandler} startColor={START_COLOR} />
+      <ColorLabel color={createStyles().labelColor} isError={isErrorColor} errorText={ERROR_TEXT} borderColor={createStyles().borderColor} textColor={createStyles().textColor} />
+    </Bacground>
   );
-}
+};
 
 export default App;
